6c0a678b1d9fd14b190aa642fcbbf821a72331b3
kpw0521 <62336424+kpw0521@users.noreply.github.com>
2020-04-09 09:41:27 +0900

    변수 a,b / 출력문 추가

--- GitTest/src/hellogit/HelloGit.java
+++ GitTest/src/hellogit/HelloGit.java
@@ -3,5 +3,11 @@ package hellogit;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("hello world");
+
+	
+		String a = " hello";
+		String b = " world";
+		
+		System.out.println(a+b);
 	}
 }
